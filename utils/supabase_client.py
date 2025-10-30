657f0fb1de9d8cb2d8b65d307c724266773b0f4b
Zealot <73077650+zealot-network@users.noreply.github.com>
2025-10-30 16:22:36 -0500

    Create supabase_client.py

---
+++ utils/supabase_client.py
@@ -0,0 +1,7 @@
+import os
+from supabase import create_client, Client
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
